fdaf019de613b6c0516c321ee60578433fc12a52
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-11-08 11:20:38 -0800

    Update overwrite prompt in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,7 +28,7 @@ clean
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	read -p "This may overwrite existing files in ~/bin. Are you sure? (y/n) " -n 1
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt
